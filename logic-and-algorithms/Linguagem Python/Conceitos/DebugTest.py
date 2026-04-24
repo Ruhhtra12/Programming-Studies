f36bc661777a16d8a3870549f713ce44dd42827b
Ruhhtra12 <arthurcastrocosta07@gmail.com>
2026-04-24 10:38:50 -0300

    feat: adicionando conceitos

---
+++ logic-and-algorithms/Linguagem Python/Conceitos/DebugTest.py
@@ -0,0 +1,4 @@
+Name = input("What's your name little boy?")
+age = input("How old are you kid?")
+Height = input("how tall are you boy?")
+print(f"So your name is {Name}, and you are {age} years old and you have {Height} Cm, that's great!")
---
+++ logic-and-algorithms/Linguagem Python/Conceitos/Matrizes.py
@@ -0,0 +1,12 @@
+n = int(input("Quantas linhas tera a matriz?"))
+m = int(input("Quantas colunas tera a matriz?"))
+mat: list[list[int]] = [[ 0 for x in range(n) ] for x in range (m)]
+
+for y in range(0,n):
+    for x in range(0,m):
+        value = input(f"Valor no ponto [{y}][{x}] = ")
+        mat[y][x] = value
+print("Operação terminada!")
+print("Matriz descrita:")
+for valores in mat:
+    print(valores)
